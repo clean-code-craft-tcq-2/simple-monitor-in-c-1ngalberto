6d69d3ef91653a68cac10f9a7d5bb4a3889ef939
Alberto Peña <ing.albertopc@outlook.com>
2022-02-08 17:42:04 -0600

    Testing, Low, High logic and compiling errors

--- checker.c
+++ checker.c
@@ -2,48 +2,56 @@
 #include <assert.h>
 
 
-#define E_OK			0;
-#define E_NOK 			1;
-#define Temp_Low		0;
-#define Temp_High		45;
-#define	Soc_Low			20;
-#define Soc_High		80;
-#define ChargeRate_Low	0;
-#define ChargeRate_High	0.8;
-#define Battery_NOK		0;
-#define Battery_OK 		1;
+#define E_OK			0
+#define E_NOK 			1
+#define Temp_Low		0
+#define Temp_High		45
+#define	Soc_Low			20
+#define Soc_High		80
+#define ChargeRate_Low	0
+#define ChargeRate_High	0.8
+#define Battery_NOK		0
+#define Battery_OK 		1
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
+enum ParamIndex{
+	Temperature,
+	Soc,
+	ChargeRate
+};
 
-  int isAllOk_f = E_OK;
-  isAllOk_f  = CheckParamIsInRange (float temperature,float Temp_Low,float Temp_High , char "Temperature");
-  isAllOk_f |= CheckParamIsInRange (float soc,float Soc_Low,float Soc_High , char "Soc");
-  isAllOk_f |= CheckParamIsInRange (float chargeRate,float ChargeRate_Low,float ChargeRate_High , char "ChargeRate");
-
-  return (isAllOk_f == E_OK) ? Battery_OK:Battery_NOK;
-}
-
-int CheckParamIsInRange (float inParamToCheck,float Lower_Limit ,float Upper_Limit , char ParamName)
+int CheckParamIsInRange (float inParamToCheck,float Lower_Limit ,float Upper_Limit , const char* ParamName)
 {
 	int Error = E_OK;
 	if(inParamToCheck < Lower_Limit )
 	{
-	    printf("%c IS LOW!\n", ParamName);
+	    printf("%s IS LOW!\n", ParamName);
 	    Error = E_NOK;
 	}
 	else if (inParamToCheck > Upper_Limit)
 	{
-		printf("%c is HIGH!\n", ParamName);
+		printf("%s is HIGH!\n", ParamName);
 	    Error = E_NOK;
 	}
 	else{
-		printf("%c is in range!\n", ParamName);
+		printf("%s is in range!\n", ParamName);
 	}
 	return Error;
 }
 
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+
+  int isAllOk_f = E_OK;
+  const char* ParamName[] = {"Temperature","Soc","ChargeRate"};
+
+  isAllOk_f  = CheckParamIsInRange ( temperature, Temp_Low, Temp_High ,ParamName[Temperature]);
+  isAllOk_f |= CheckParamIsInRange ( soc, Soc_Low, Soc_High , ParamName[Soc]);
+  isAllOk_f |= CheckParamIsInRange ( chargeRate, ChargeRate_Low, ChargeRate_High , ParamName[ChargeRate]);
+
+  return isAllOk_f == E_OK ? Battery_OK:Battery_NOK;
+}
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  assert(!batteryIsOk(50, 85, 1));
+  assert(!batteryIsOk(-15, 10, -1));
 }

1ee6145d41c823d9b3bbd5562f9385878e883710
Alberto Peña <ing.albertopc@outlook.com>
2022-02-08 16:21:29 -0600

    separate functions

--- checker.c
+++ checker.c
@@ -1,19 +1,63 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+#define E_OK			0;
+#define E_NOK 			1;
+#define Temp_Low		0;
+#define Temp_High		45;
+#define	Soc_Low			20;
+#define Soc_High		80;
+#define ChargeRate_High	0.8;
+#define Battery_NOK		0;
+#define Battery_OK 		1;
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+
+  int isAllOk_f = E_OK;
+  isAllOk_f  = isTemperatureInRange (float temperature);
+  isAllOk_f |= isSocInRange (float soc);
+  isAllOk_f |= isChargeRateInRange (float chargeRate);
+
+  return (isAllOk_f == E_OK) ? Battery_OK:Battery_NOK;
 }
+
+int isTemperatureInRange (float temperature)
+{
+	int Error = E_OK;
+	if(temperature < Temp_Low || temperature > Temp_High)
+	{
+	    printf("Temperature out of range!\n");
+	    Error = E_NOK;
+	}
+	else{}
+	return Error;
+}
+
+int isSocInRange (float soc)
+{
+	int Error = E_OK;
+	if(soc < Soc_Low || soc > Soc_High)
+	{
+		printf("State of Charge out of range!\n");
+		Error = E_NOK;
+	}
+	else{}
+	return Error;
+}
+
+int isChargeRateInRange (float chargeRate)
+{
+	int Error = E_OK;
+	if(chargeRate > ChargeRate_High)
+	{
+	    printf("Charge Rate out of range!\n");
+	    Error = E_NOK;
+	}
+	else{}
+	return Error;
+}
+
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
